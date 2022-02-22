ce451433e8ed5e3d387230767efe008c57ce945c
le pavec malo <malo.lepavec@gmail.com>
2022-02-22 10:54:36 +0100

    ok

--- Initiation c++/Initiation c++.cpp
+++ Initiation c++/Initiation c++.cpp
@@ -7,6 +7,33 @@
 
 using namespace std;
 
+
+
+double Add(double a, double b)
+{
+	return a + b;
+}
+
+double Substract(double a, double b)
+{
+	return a - b;
+}
+
+double Multiply(double a, double b)
+{
+	return a * b;
+}
+
+double Divide(double a, double b)
+{
+	return a / b;
+}
+
+double DivideRest(int a, int b)
+{
+	return a % b;
+}
+
 int main()
 {
 	vector<string> s;
@@ -14,6 +41,7 @@ int main()
 	s.push_back("2 soustraire");
 	s.push_back("3 multiplier");
 	s.push_back("4 diviser");
+	s.push_back("5 division euclidienne");
 
 	for (int i = 0; i < s.size(); i++)
 	{
@@ -31,55 +59,43 @@ int main()
 	c = stod(a);
 	d = stod(b);
 
-	double res = 0.00;
-	if (stod(choix) == 1)
+	double res;
+	
+	
+	int line = stoi(choix);
+
+	switch (line)
 	{
-		res = Add(c,d);
+	case 1:
+		res = Add(c, d);
+		break;
+
+	case 2:
+		res = Substract(c, d);
+		break;
+
+	case 3:
+		res = Multiply(c, d);
+		break;
+
+	case 4:
+		res = Divide(c, d);
+		break;
+
+	case 5:
+		res = DivideRest(c, d);
+		break;
+
+	default:
+		res = 0.00;
+		break;
 	}
-	else if (stod(choix) == 2)
-		res = Substract(c,d);
-	else if (stod(choix) == 3)
-		res = Multiply(c,d);
-	else if (stod(choix) == 4)
-		res = Divide(c,d);
 	
 	cout << res << endl;
 	
 	return 0;
 }
 
-int Add(double a,double b)
-{
-	return a + b;
-}
 
-int Substract(double a, double b)
-{
-	return a - b;
-}
 
-int Multiply(double a, double b)
-{
-	return a * b;
-}
 
-int Divide(double a, double b)
-{
-	return a / b;
-}
-
-int DivideRest(int a, int b)
-{
-	return a % b;
-}
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
